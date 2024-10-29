6704a7bc288e41789a75b101301bcce650194de6
icarogz <icaro.sousa19@aluno.ce.gov.br>
2024-10-29 14:52:01 -0300

    Add files via upload

---
+++ Page.php
@@ -0,0 +1,12 @@
+<?php include 'config.php'; ?>
+
+<?php include 'includes/funcions/sessionstart.php'; ?>
+
+<?php include 'includes/body/header.php'; ?>
+
+<?php include 'includes/body/body.php'; ?>
+
+<?php include 'includes/body/footer.php'; ?>
+
+
+
---
+++ cadastro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadastro</title>
+    <style>
+        .form{
+            background-color: #9AD1E4;
+            border-radius: 15px;
+            padding-bottom: 50px;
+        }
+
+
+    </style>
+</head>
+<body>
+    <center>
+    <div class="form">
+        <h1 id="titulo">cadastre-se</h1>
+    <form action="POST"> 
+        <input type="name" name="nome" placeholder="usuario">
+
+
+    </form>
+    </div>
+    </center>
+</body>
+</html>
