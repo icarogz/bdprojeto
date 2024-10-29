5e7b3c8a35086fc162b2b0c7b4faeda0708c08d2
icarogz <icaro.sousa19@aluno.ce.gov.br>
2024-10-29 20:20:42 -0300

    Update index1.php

--- index1.php
+++ index1.php
@@ -57,7 +57,7 @@
                             <a href="about.html" class="nav-item nav-link">Sobre</a>
                             <a href="service.html" class="nav-item nav-link">Jogos</a>
                             
-                            <a href="contact.html" class="nav-item nav-link">Contact</a>
+                            <a href="contact.html" class="nav-item nav-link">Contato</a>
                            
                 </nav>
             </div>
@@ -73,15 +73,15 @@
                         <div class="row g-4 align-items-center">
                             <div class="col-lg-7 animated fadeInLeft">
                                 <div class="text-sm-center text-md-start">
-                                    <h4 class="text-white text-uppercase fw-bold mb-4">BEM VINDO </h4>
-                                    <h1 class="display-1 text-white mb-4">Life Insurance Makes You Happy</h1>
-                                    <p class="mb-5 fs-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy... 
+                                    <h4 class="text-white text-uppercase fw-bold mb-4">BEM VINDO, </h4>
+                                    <h1 class="display-1 text-white mb-4">Fulano!</h1>
+                                    <p class="mb-5 fs-5">Jogos pensados para encantar, estimular e apoiar o desenvolvimento de cada criança!
                                     </p>
                                 </div>
                             </div>
                             <div class="col-lg-5 animated fadeInRight">
                                 <div class="calrousel-img" style="object-fit: cover;">
-                                    <img src="img/carousel-2.png" class="img-fluid w-100" alt="">
+                                    <img src="img/imginicio1.png" class="img-fluid w-100" alt="">
                                 </div>
                             </div>
                         </div>
@@ -94,14 +94,14 @@
                         <div class="row gy-4 gy-lg-0 gx-0 gx-lg-5 align-items-center">
                             <div class="col-lg-5 animated fadeInLeft">
                                 <div class="calrousel-img">
-                                    <img src="img/carousel-2.png" class="img-fluid w-100" alt="">
+                                    <img src="img/imginicio1.png" class="img-fluid w-100" alt="">
                                 </div>
                             </div>
                             <div class="col-lg-7 animated fadeInRight">
                                 <div class="text-sm-center text-md-end">
-                                    <h4 class="text-white text-uppercase fw-bold mb-4">BEM VINDO</h4>
-                                    <h1 class="display-1 text-white mb-4">Life Insurance Makes You Happy</h1>
-                                    <p class="mb-5 fs-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy... 
+                                    <h4 class="text-white text-uppercase fw-bold mb-4">BEM VINDO,</h4>
+                                    <h1 class="display-1 text-white mb-4">Fulano!</h1>
+                                    <p class="mb-5 fs-5">Jogos pensados para encantar, estimular e apoiar o desenvolvimento de cada criança!
                                     </p>
                                     
                                 </div>
@@ -117,9 +117,9 @@
         <div class="container-fluid feature bg-light py-5">
             <div class="container py-5">
                 <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
-                    <h4 class="text-primary">Our Features</h4>
-                    <h1 class="display-4 mb-4">Insurance Provide you a Better Future</h1>
-                    <p class="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur adipisci facilis cupiditate recusandae aperiam temporibus corporis itaque quis facere, numquam, ad culpa deserunt sint dolorem autem obcaecati, ipsam mollitia hic.
+                    <h1 class="display-4 mb-4">Nossos serviços</h1>
+                    <p class="mb-0">Oferecemos uma variedade de projetos desenvolvidos especialmente para estimular
+                    habilidades, proporcionando uma experiência inclusiva e divertida para cada criança. Dentre estas, são:
                     </p>
                 </div>
                 <div class="row g-4">
@@ -128,10 +128,8 @@
                             <div class="feature-icon p-4 mb-4">
                                 <i class="far fa-handshake fa-3x"></i>
                             </div>
-                            <h4 class="mb-4">Trusted Company</h4>
-                            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic laborum odit pariatur...
-                            </p>
-                            <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
+                            <h4 class="mb-4">Desenvolver habilidades cognitivas, motoras e sociais</h4>
+                            
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.4s">
@@ -139,10 +137,8 @@
                             <div class="feature-icon p-4 mb-4">
                                 <i class="fa fa-dollar-sign fa-3x"></i>
                             </div>
-                            <h4 class="mb-4">Anytime Money Back</h4>
-                            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic laborum odit pariatur...
-                            </p>
-                            <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
+                            <h4 class="mb-4">Trabalhar a inclusão</h4>
+                         
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.6s">
@@ -150,10 +146,8 @@
                             <div class="feature-icon p-4 mb-4">
                                 <i class="fa fa-bullseye fa-3x"></i>
                             </div>
-                            <h4 class="mb-4">Flexible Plans</h4>
-                            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic laborum odit pariatur...
-                            </p>
-                            <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
+                            <h4 class="mb-4">Oferecer jogos simples, objetivos e divertidos</h4>
+                          
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.8s">
@@ -161,10 +155,8 @@
                             <div class="feature-icon p-4 mb-4">
                                 <i class="fa fa-headphones fa-3x"></i>
                             </div>
-                            <h4 class="mb-4">24/7 Fast Support</h4>
-                            <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic laborum odit pariatur...
-                            </p>
-                            <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a>
+                            <h4 class="mb-4">Trazer suporte às crianças durante as atividades</h4>
+                      
                         </div>
                     </div>
                 </div>
